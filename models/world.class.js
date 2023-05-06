ae04d285838bc9dd4381e445ff8722c6b5d7500d
Leo Albert <leo.albert.96@web.de>
2023-05-06 13:01:16 +0200

    draw method implemented

--- models/world.class.js
+++ models/world.class.js
@@ -6,9 +6,19 @@ enemies = [
     new Chicken(),
 ];
 
+ctx;
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
 
     draw(){
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
+        for (let i = 0; i < this.enemies.length; i++) {
+            const enemy = this.enemies[i];
+            this.ctx.drawImage(enemy.img, enemy.x +300, enemy.y, enemy.width, enemy.height);
+        }
     }
 }
