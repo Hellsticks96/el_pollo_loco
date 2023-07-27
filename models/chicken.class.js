862e12139ff704d78e9b3cd08bd5d7aa191f70a9
Leo Albert <leo.albert.96@web.de>
2023-07-27 12:56:54 +0200

    now all imgs loading

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,12 +2,26 @@ class Chicken extends MovableObject{
     height = 80;
     width = 80;
     y = 560;
+    
 
 
     constructor(){
         super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 800 + Math.random() * 200;
+        this.animate()
     }
 
+    animate(){
+        let i = 2;
+        setInterval(()=>{
+            if (i >= 3 ) {
+                i = 1;
+            }
+            this.x -= 0.25
+            loadImage(`../img/3_enemies_chicken/chicken_normal/${i}_walk/_w.png`);
+            i++;
+            
+        }, 1000 / 60);
+    }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,7 @@
 class World{
 character = new Character();
 backgrounds = [
+    new Background('../img/5_background/layers/air.png'),
     new Background('../img/5_background/layers/3_third_layer/1.png'),
     new Background('../img/5_background/layers/2_second_layer/1.png'),
     new Background('../img/5_background/layers/1_first_layer/1.png'), 
