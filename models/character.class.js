05df809c022db14f6f3cb607cbf0c36ab2fe4aee
Leo Albert <leo.albert.96@web.de>
2023-07-27 14:16:50 +0200

    img cache now loading.started code for animations

--- models/character.class.js
+++ models/character.class.js
@@ -3,9 +3,58 @@ class Character extends MovableObject{
     width = 250;
     y = 200;
     x = 70;
+    IMAGES_WALKING = 
+    ['../img/2_character_pepe/2_walk/W-21.png',
+    '../img/2_character_pepe/2_walk/W-22.png',
+    '../img/2_character_pepe/2_walk/W-23.png',
+    '../img/2_character_pepe/2_walk/W-24.png',
+    '../img/2_character_pepe/2_walk/W-25.png',
+    '../img/2_character_pepe/2_walk/W-26.png'
+    ];
+
+    IMAGES_SHORT_IDLE =
+    ['../img/2_character_pepe/1_idle/idle/I-1.png',
+    '../img/2_character_pepe/1_idle/idle/I-2.png',
+    '../img/2_character_pepe/1_idle/idle/I-3.png',
+    '../img/2_character_pepe/1_idle/idle/I-4.png',
+    '../img/2_character_pepe/1_idle/idle/I-5.png',
+    '../img/2_character_pepe/1_idle/idle/I-6.png',
+    '../img/2_character_pepe/1_idle/idle/I-7.png',
+    '../img/2_character_pepe/1_idle/idle/I-8.png',
+    '../img/2_character_pepe/1_idle/idle/I-9.png',
+    '../img/2_character_pepe/1_idle/idle/I-10.png',
+    ];
+
+    IMAGES_LONG_IDLE = [
+        '../img/2_character_pepe/1_idle/long_idle/I-11.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-12.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-13.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-14.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-15.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-16.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-17.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-18.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-19.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-20.png',
+    ];
+
+    currentImage = 0;
 
     constructor(){
         super().loadImage('../img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_SHORT_IDLE);
+        this.loadImages(this.IMAGES_LONG_IDLE);
+        this.animate();
+    }
+
+    animate(){
+        setInterval( () => {
+            let i = this.currentImage % this.IMAGES_LONG_IDLE.length;
+            let path = this.IMAGES_LONG_IDLE[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 1000 / 6);
     }
 
     jump(){
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -9,19 +9,5 @@ class Chicken extends MovableObject{
         super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 800 + Math.random() * 200;
-        this.animate()
-    }
-
-    animate(){
-        let i = 2;
-        setInterval(()=>{
-            if (i >= 3 ) {
-                i = 1;
-            }
-            this.x -= 0.25
-            loadImage(`../img/3_enemies_chicken/chicken_normal/${i}_walk/_w.png`);
-            i++;
-            
-        }, 1000 / 60);
     }
 }
